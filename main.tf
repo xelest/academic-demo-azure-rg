912342333cdeaa72f373b7f05de7da59adb7b213
manthonyh <aemccoo@live.com>
2026-06-26 10:45:28 -0700

    test
    Signed-off-by: manthonyh <aemccoo@live.com>

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "demo" {
-  name     = var.resource_group_name
-  location = var.location
-  tags     = var.tags
-}
+# resource "azurerm_resource_group" "demo" {
+#   name     = var.resource_group_name
+#   location = var.location
+#   tags     = var.tags
+# }
 
-resource "azurerm_resource_group" "demo2" {
-  name     = "rg-demo2"
-  location = "westus"
-  tags     = var.tags
-}
-
+# resource "azurerm_resource_group" "demo2" {
+#   name     = "rg-demo2"
+#   location = "westus"
+#   tags     = var.tags
+# }
+# # 
 
