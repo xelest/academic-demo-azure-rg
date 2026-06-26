01c5113d99b41d166487bc27458392683d14fc2f
manthonyh <aemccoo@live.com>
2026-06-26 10:29:54 -0700

    this is a test
    Signed-off-by: manthonyh <aemccoo@live.com>

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.14.0"
+  required_version = "~> 1.14.8"
 
   required_providers {
     azurerm = {
@@ -12,4 +12,26 @@ terraform {
 # Terraform Cloud supplies Azure credentials through workspace environment variables.
 provider "azurerm" {
   features {}
+}
+
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "homelab-azure"
+#     workspace = {
+#       name = "academic-demo-azure-rg"
+#     }
+#   }
+# }
+
+
+terraform {
+  cloud {
+    organization = "homelab-azure"
+
+    workspaces {
+      name = "academic-demo-azure-rg"
+    }
+
+  }
 }
